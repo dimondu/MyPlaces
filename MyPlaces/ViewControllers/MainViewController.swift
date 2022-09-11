8b245c6e7c95379df518663481949282a58be65c
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-09-11 10:45:21 +0300

    Prepare for sorting

---
+++ MyPlaces/ViewControllers/MainViewController.swift
@@ -0,0 +1,83 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Дуров on 03.09.2022.
+//
+
+import UIKit
+import RealmSwift
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Public properties
+    
+    var places: Results<Place>!
+    
+    // MARK: - Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        places = realm.objects(Place.self)
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let place = places[indexPath.row]
+            guard let newPlaceVC = segue.destination as? NewPlaceViewController else { return }
+            newPlaceVC.currentPlace = place
+        }
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
+        newPlaceVC.savePlace()
+        tableView.reloadData()
+    }
+}
+
+// MARK: - Table view data source
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return places.isEmpty ? 0 : places.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell else { return UITableViewCell() }
+        
+        let place = places[indexPath.row]
+        
+        cell.nameLabel.text = place.name
+        cell.locationLabel.text = place.location
+        cell.typeLabel.text = place.type
+        cell.imageOfPlace.image = UIImage(data: place.imageData ?? Data())
+        
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        
+        return cell
+    }
+}
+
+// MARK: - Table view delegate
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            let place = places[indexPath.row]
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
