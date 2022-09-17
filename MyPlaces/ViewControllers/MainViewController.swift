3330e6856cfb2fce6842a7689318abc2e06d369f
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-09-17 13:56:50 +0300

    Custom rating control part 2

--- MyPlaces/ViewControllers/MainViewController.swift
+++ MyPlaces/ViewControllers/MainViewController.swift
@@ -141,6 +141,10 @@ extension MainViewController: UITableViewDelegate {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
 // MARK: - UISearchResultsUpdating
